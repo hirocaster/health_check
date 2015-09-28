89cb20f286bfa7c1b296163144f79cda888499bf
hirocaster <hohtsuka@gmail.com>
2015-09-28 16:35:15 +0900

    Add faild case for reids plugin spec

--- spec/lib/health_check/plugins/redis_spec.rb
+++ spec/lib/health_check/plugins/redis_spec.rb
@@ -7,4 +7,19 @@ describe "HealthCheck::Plugins::Redis" do
     }.not_to raise_error
   end
 
+  context "Redis returns other value" do
+    before do
+      redis = ::Redis.new
+      allow(redis).to receive(:get).and_return "faild value"
+      allow(::Redis).to receive(:new).and_return redis
+    end
+
+    it "raise different values exception" do
+      expect {
+        redis = HealthCheck::Plugins::Redis.new
+        redis.check!
+      }.to raise_error HealthCheck::Plugins::RedisException
+    end
+  end
+
 end
