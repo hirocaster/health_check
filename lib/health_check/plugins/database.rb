1b4686907ad5a248b196cc34e7bafc14dc488da1
hirocaster <hohtsuka@gmail.com>
2015-10-23 09:02:54 +0900

    Refactor database plugin

--- lib/health_check/plugins/database.rb
+++ lib/health_check/plugins/database.rb
@@ -4,24 +4,28 @@ module HealthCheck
 
     class Database < Base
       def check!(request: nil)
-        @check_klasses = []
-
-        if request && request.params[:database_check_classes]
-          request.params[:database_check_classes].split(",").each do |klass_string|
-            klass = klass_string.constantize
-            @check_klasses << klass
-          end
-        else
-          @check_klasses << ActiveRecord::Base
-        end
-
         super
 
-        @check_klasses.each do |check_klass|
+        check_klasses.each do |check_klass|
           ActiveRecord::Migrator.current_version check_klass.connection
         end
       rescue StandardError => e
         raise DatabaseException, e.message
+      end
+
+      private
+
+      def check_klasses(request)
+        result = []
+        if request && request.params[:database_check_classes]
+          request.params[:database_check_classes].split(",").each do |klass_string|
+            klass = klass_string.constantize
+            result << klass
+          end
+        else
+          result << ActiveRecord::Base
+        end
+        result
       end
     end
   end
